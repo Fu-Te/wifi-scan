6f9453ca1b29d69f7ef4c2631c38ae26f632ca84
Fu-Te <37261985+Fu-Te@users.noreply.github.com>
2022-03-30 15:35:52 +0900

    add

---
+++ wifi_scan.py
@@ -0,0 +1,55 @@
+from scapy.all import *
+from threading import Thread
+import pandas as pd
+import time
+import os
+
+#ネットワークを格納する空のデータフレーム
+networks = pd.DataFrame(columns=['BSSID','SSID','dBm_Signal','Channel','Crypto'])
+networks.set_index('BSSID',inplace=True)
+
+def callback(packet):
+    if packet.haslayer(Dot11Beacon):
+        bssid=packet[Dot11].addr2
+        ssid=packet[Dot11Elt].info.decode()
+        
+        try:
+            dbm_signal=packet.dBm_AntSignal
+        except:
+            dbm_signal='N/A'
+            
+        stats = packet[Dot11Beacon].network_stats()
+        
+        channel = stats.get('channel')
+        
+        crypto = stats.get('crypto')
+        
+        networks.loc[bssid]=(ssid,dbm_signal,channel,crypto)
+        
+        
+def print_all():
+    while True:
+        os.system('clear')
+        print(networks)
+        time.sleep(0.5)
+        
+def change_channel():
+    ch =1
+    while True:
+        os.system(f'iwconfig{interface} channel {ch}')
+        
+        ch=ch%14+1
+        time.sleep(0.5)        
+if __name__ == '__main__':
+    interface='en0'
+    
+    printer=Thread(target=print_all)
+    printer.daemon=True
+    printer.start()
+    
+    channel_changer = Thread(target_change_channel)
+    channel_changer.daemon = True
+    channel_changer.start()
+    
+    
+    sniff(prn=callback,iface=interface)
